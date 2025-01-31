387afdce7bdb9fb352469603536708ebc7e9ee1e
Mark Friedenbach <mark@friedenbach.org>
2025-01-31 14:22:33 -0800

    Add text of existing landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
+    <div className="prose mx-auto max-w-screen-sm font-[family-name:var(--font-geist-sans)]">
+      <div className="mt-[70px]">
+        <h2>Machine Phase Systems Inc.</h2>
+        <p><b>Molecular nanotechnology is within reach.</b></p>
+        <p>Building scalable, mature molecular nanotechnology is the most important technical challenge of our time, offering the potential to close the gaps between existing capabilities and physical limits in nearly every technological domain.</p>
+        <p>With radical advancements especially in the core domains of computing, sensing, and materials science, the downstream impact of advanced nanotechnology will be felt by every industry and will forever change the material basis of civilization.</p>
+        <p>We have started a straight-shot molecular nanotechnology lab with one overriding goal: bootstrapping our way to a mature, high-throughput atomically precise manufacturing, using commercially available off-the-shelf hardware and the mature experimental techniques of synthetic chemistry and surface science laboratories.</p>
+        <p>We are Machine Phase Systems Inc.</p>
+        <p>The vision of molecular nanotechnology that has captivated technologists and futurists since the 1980’s is now within reach, and we intend to grab hold of it first. Our team, investors, and business model are all aligned to achieve this goal.</p>
+        <p>We are an American company with top technical talent acquired from across the globe.</p>
+        <p>We are assembling a lean, cracked team of highly competent engineers and scientists dedicated to the goal of achieving repeatable, reliable machine-phase chemistry, and scaling up these processes to create a system for high-throughput atomically precise manufacturing.</p>
+        <p>If that’s you, we offer an opportunity to do your life’s work and help usher in a new technological epoch, a transformation that will permanently alter the foundational technologies upon which society is built.</p>
+        <p>Now is the time. Join us.</p>
+        <p className="pt-[20px]">December 5, 2024</p>
+        <p><a href="mailto:hello@machinephase.systems">hello@machinephase.systems</a></p>
+      </div>
     </div>
   );
 }
