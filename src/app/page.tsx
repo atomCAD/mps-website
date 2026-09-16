a0047a1c0502ed70fe6ef7ee55020252848599b9
Mark Friedenbach <mark@friedenbach.org>
2026-09-16 00:25:45 -0700

    Remove outdated teaser text.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,6 @@ export default function Home() {
     <div className="prose mx-auto max-w-screen-sm font-[family-name:var(--font-roboto-sans)]">
       <div className="mt-[70px]">
         <h2>Machine Phase Systems Inc.</h2>
-        <p><b>Molecular nanotechnology is within reach.</b></p>
-        <p>Building scalable, mature molecular nanotechnology is the most important technical challenge of our time, offering the potential to close the gaps between existing capabilities and physical limits in nearly every technological domain.</p>
-        <p>With radical advancements especially in the core domains of computing, sensing, and materials science, the downstream impact of advanced nanotechnology will be felt by every industry and will forever change the material basis of civilization.</p>
-        <p>We have started a straight-shot molecular nanotechnology lab with one overriding goal: bootstrapping our way to a mature, high-throughput atomically precise manufacturing, using commercially available off-the-shelf hardware and the mature experimental techniques of synthetic chemistry and surface science laboratories.</p>
-        <p>We are Machine Phase Systems Inc.</p>
-        <p>The vision of molecular nanotechnology that has captivated technologists and futurists since the 1980’s is now within reach, and we intend to grab hold of it first. Our team, investors, and business model are all aligned to achieve this goal.</p>
-        <p>We are an American company with top technical talent acquired from across the globe.</p>
-        <p>We are assembling a lean, cracked team of highly competent engineers and scientists dedicated to the goal of achieving repeatable, reliable machine-phase chemistry, and scaling up these processes to create a system for high-throughput atomically precise manufacturing.</p>
-        <p>If that’s you, we offer an opportunity to do your life’s work and help usher in a new technological epoch, a transformation that will permanently alter the foundational technologies upon which society is built.</p>
-        <p>Now is the time. Join us.</p>
         <p><a href="mailto:hello@machinephase.systems">hello@machinephase.systems</a></p>
       </div>
     </div>
